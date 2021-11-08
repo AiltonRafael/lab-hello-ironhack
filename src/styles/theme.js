df6d02862454c2623655456cd8c9d51db9a5934a
Ailton Rafael <ailton.junior@al.infnet.edu.br>
2021-11-08 15:54:35 -0300

    updated

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+import { extendTheme } from '@chakra-ui/react';
+
+ export const theme = extendTheme({
+      styles: {
+        global: {  
+        'html': {
+              height: "100vh"
+          },
+          'body': {
+            maxHeight: '100%',
+          }
+        }
+      }
+ })
